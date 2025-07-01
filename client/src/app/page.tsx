f978ddf7d476516089c0e86cb38c799a6d7d24fa
rozengoza <rozengoza22@gmail.com>
2025-07-01 22:16:53 +0545

    SectionContainer added - Build failed through ESLinting

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Hero34 } from "@/components/hero34";
 import { Feature74 } from "@/components/feature74";
-import { Gallery6 } from "@/components/gallery6";
+// import { Gallery6 } from "@/components/gallery6";
 export default function Home() {
   return (
     <>
